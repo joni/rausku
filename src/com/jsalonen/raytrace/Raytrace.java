3b15d6a388ed7442f0bb70920e5064eb3d688682
Joni Salonen <joni.salonen@gmail.com>
2018-08-12 10:06:53 -0400

    add scene. add ray. add ground. display result.

--- src/com/jsalonen/raytrace/Raytrace.java
+++ src/com/jsalonen/raytrace/Raytrace.java
@@ -1,19 +1,130 @@
 package com.jsalonen.raytrace;
 
 import javax.swing.*;
+import java.awt.image.BufferedImage;
 
 public class Raytrace {
     public static void main(String... args) {
+        int pixelWidth = 256;
+        int pixelHeight = 256;
+        Canvas canvas = new Canvas(pixelWidth, pixelHeight, 64, Vec.of(0, 0, 0), 1);
+
+        Scene scene = new Scene();
+
+        BufferedImage image = new BufferedImage(pixelWidth, pixelHeight, BufferedImage.TYPE_INT_RGB);
+        for (int y = 0; y < pixelHeight; y++) {
+            for (int x = 0; x < pixelWidth; x++) {
+                Ray ray = canvas.getRayFromOriginToCanvas(x, y);
+
+                Color color = scene.resolveRayColor(ray);
+
+                image.setRGB(x, y, color.toIntRGB());
+            }
+        }
+
+
         SwingUtilities.invokeLater(() -> {
                     JFrame frame = new JFrame();
                     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                    JLabel label = new JLabel("hello world");
+            JLabel label = new JLabel(new ImageIcon(image));
                     frame.add(label);
                     frame.pack();
                     frame.setVisible(true);
                 }
         );
     }
+}
+
+class Scene {
+
+    Color resolveRayColor(Ray ray) {
+        Vec direction = ray.getDirection();
+
+        if (direction.y < 0) {
+            // ground hit
+            return Color.of(1, 1, 1);
+        }
+
+        // nothing hit
+        return Color.of(0, 0, 0);
+    }
+}
+
+class Canvas {
+
+    float sceneWidth;
+    float sceneHeight;
+    float dotsPerUnit;
+    Vec origin;
+    float distance;
+
+    int pixelWidth;
+    int pixelHeight;
+
+    public Canvas(int pixelWidth, int pixelHeight, float dotsPerUnit, Vec origin, float distance) {
+        this.sceneWidth = pixelWidth / dotsPerUnit;
+        this.sceneHeight = pixelHeight / dotsPerUnit;
+        this.dotsPerUnit = dotsPerUnit;
+        this.origin = origin;
+        this.distance = distance;
+        this.pixelWidth = pixelWidth;
+        this.pixelHeight = pixelHeight;
+    }
+
+    public Ray getRayFromOriginToCanvas(int pixelX, int pixelY) {
+        float canvasX = (pixelX - pixelWidth / 2) / dotsPerUnit;
+        float canvasY = (pixelHeight / 2 - pixelY) / dotsPerUnit;
+        float canvasZ = distance;
+        Vec canvasPoint = Vec.of(canvasX, canvasY, canvasZ);
+        return Ray.from(origin, canvasPoint);
+    }
+}
+
+class Ray {
+
+    private final Vec origin;
+    private final Vec direction;
+
+    public Ray(Vec origin, Vec direction) {
+        this.origin = origin;
+        this.direction = direction;
+    }
+
+    public static Ray from(Vec origin, Vec canvasPoint) {
+        return new Ray(origin, canvasPoint);
+    }
+
+    public Vec getDirection() {
+        return direction;
+    }
+}
+
+class Color {
+    float r;
+    float g;
+    float b;
+
+    public Color(int r, int g, int b) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    public static Color of(int i, int i1, int i2) {
+        return new Color(i, i1, i2);
+    }
+
+    public int toIntRGB() {
+        int r = Math.min(255, (int) (256 * this.r));
+        int g = Math.min(255, (int) (256 * this.g));
+        int b = Math.min(255, (int) (256 * this.b));
+        return (0xff << 24) | (r << 16) | (g << 8) | b;
+    }
+}
+
+class Sphere {
+    Vec center;
+    float radius;
 }
 
 class Vec {
@@ -30,6 +141,12 @@ class Vec {
         x = v.x;
         y = v.y;
         z = v.z;
+    }
+
+    public Vec(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
     public Vec add(Vec v) {
@@ -71,5 +188,14 @@ class Vec {
     public Vec normalize() {
         float len = (float) Math.sqrt(sqLen());
         return div(len);
+    }
+
+    public static Vec of(float x, float y, float z) {
+        return new Vec(x, y, z);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Vec(%f, %f, %f)", x, y, z);
     }
 }
