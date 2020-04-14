94a9851015a2e9aea0d5b811e29e0aae382d1801
Joni Salonen <joni.salonen@gmail.com>
2020-04-13 21:55:15 -0400

    Add menubar, with File menu and save and exit actions

--- src/rausku/Raytrace.java
+++ src/rausku/Raytrace.java
@@ -2,11 +2,14 @@ package rausku;
 
 import rausku.scenes.Scene2;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Raytrace {
 
@@ -34,20 +37,37 @@ public class Raytrace {
         scene.debug = true;
 
         SwingUtilities.invokeLater(() -> {
-                    JFrame frame = new JFrame();
-                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                    JLabel label = new JLabel(new ImageIcon(image));
-                    label.addMouseListener(new MouseAdapter() {
-                        @Override
-                        public void mouseClicked(MouseEvent e) {
-                            Point point = e.getPoint();
-                            Ray ray = camera.getRayFromOriginToCanvas(point.x, point.y);
+            JFrame frame = new JFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            JMenuBar menubar = new JMenuBar();
+            frame.setJMenuBar(menubar);
+            JMenu fileMenu = new JMenu("File");
+            menubar.add(fileMenu);
+            fileMenu.add("Save").addActionListener(actionEvent -> {
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setDialogType(JFileChooser.SAVE_DIALOG);
+                int save = fileChooser.showDialog(frame, "Save");
+                if (save == JFileChooser.APPROVE_OPTION) {
+                    try {
+                        ImageIO.write(image, "PNG", fileChooser.getSelectedFile());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            fileMenu.add("Exit").addActionListener(actionEvent -> {
+                frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+            });
 
-                            scene.resolveRayColor(1, ray);
-
-                            //System.exit(0);
-                        }
-                    });
+            JLabel label = new JLabel(new ImageIcon(image));
+            label.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    Point point = e.getPoint();
+                    Ray ray = camera.getRayFromOriginToCanvas(point.x, point.y);
+                    scene.resolveRayColor(1, ray);
+                }
+            });
                     frame.add(label);
                     frame.pack();
                     frame.setVisible(true);
